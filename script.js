5477a2fceff21e6273f81837d4ae24b1cc756efa
Nathalie Trefz <nt@gocept.com>
2021-02-05 13:28:26 +0100

    implements md to html parser

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function time() {
 fetch('tasks.md')
   .then(response => response.text())
   .then(text => test(text));
+
+//needs script to load parser as shown in index.html
+//parse markdown format to html
+function test(placeholder) {
+    console.log("nicht gerendert " + placeholder);
+    var md = window.markdownit();
+    var result = md.render(placeholder);
+    console.log("gerendert " + result);
+}
